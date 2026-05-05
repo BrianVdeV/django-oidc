15fa0814d1aa59aa2b9564dd13beb42af405fc53
Brian <jorgebrianvdv@gmail.com>
2026-05-05 15:42:11 -0500

    staticos y docker

--- config/settings.py
+++ config/settings.py
@@ -43,6 +43,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -80,8 +81,8 @@ DATABASES = {
         'NAME': 'oidc',
         'USER': 'mariadb',
         'PASSWORD': 'mariadb',
-        'HOST': 'localhost',
-        'PORT': '3308',
+        'HOST': 'db',
+        'PORT': '3306',
     }
 }
 
@@ -121,3 +122,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
 
 STATIC_URL = 'static/'
+STATIC_ROOT = BASE_DIR / 'staticfiles'
+
+STATICFILES_DIRS = [
+    BASE_DIR / 'static'
+]
+
+OIDC_USERINFO = 'auth.apps.AuthConfig.oidc.userinfo'
