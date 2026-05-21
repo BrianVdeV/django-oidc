af22173881b9d5f8a1c2cb672eca81f687991553
Brian <jorgebrianvdv@gmail.com>
2026-05-21 12:22:07 -0500

    feat: che

--- auth/urls.py
+++ auth/urls.py
@@ -2,6 +2,6 @@ from django.urls import path, include
 from . import views
 
 urlpatterns = [
-    path("login/", views.LoginView.as_view(), name="login"),
+    path("", views.LoginView.as_view(), name="login"),
     path('openid/', include('oidc_provider.urls', namespace='oidc_provider')),
 ]
--- auth/views.py
+++ auth/views.py
@@ -1,4 +1,6 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth import authenticate, login
+from django.contrib import messages
 from django.views import View
 
 
@@ -7,4 +9,18 @@ class LoginView(View):
     template_name = "login.html"
 
     def get(self, request):
+        if request.user.is_authenticated:
+            return redirect('https://conoce.planosperu.com.pe/')
         return render(request, self.template_name)
+
+    def post(self, request):
+        username = request.POST.get('username')
+        password = request.POST.get('password')
+        user = authenticate(request, username=username, password=password)
+
+        if user is not None:
+            login(request, user)
+            return redirect('https://conoce.planosperu.com.pe/')
+        else:
+            messages.error(request, "Usuario o contraseña incorrectos.")
+            return render(request, self.template_name)
